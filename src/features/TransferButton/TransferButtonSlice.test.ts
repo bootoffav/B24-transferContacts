b77e4a478c2eda375e9d07c87eed6d470fb9ce57
Aleksei Butov <bootoffav@gmail.com>
2023-03-21 15:09:21 +0300

    Refactor tests

--- src/features/TransferButton/TransferButtonSlice.test.ts
+++ src/features/TransferButton/TransferButtonSlice.test.ts
@@ -1,10 +1,10 @@
-import { store } from "../../app/store";
 import transferButtonSlice, {
   setTransferEntityType,
   setTransferType,
   resetState,
 } from "./TransferButtonSlice";
 
+const { reducer } = transferButtonSlice;
 describe("check TransferButtonSlice of redux store", function () {
   test("checks initial state set properly defined", function () {
     const { transferEntityType, transferType } =
@@ -15,59 +15,43 @@ describe("check TransferButtonSlice of redux store", function () {
   });
 
   test("state is properly reset", function () {
-    store.dispatch(setTransferEntityType("deals"));
-    store.dispatch(resetState());
-    expect(store.getState().transferButton).toEqual(
-      transferButtonSlice.getInitialState()
-    );
+    const res = reducer(undefined, setTransferEntityType("deals"));
+    const testableResult = reducer(res, resetState());
+    expect(testableResult).toEqual(transferButtonSlice.getInitialState());
   });
 });
 
 describe("check transferEntityType behaviour", function () {
   test("checks correct transferEntityTypes can be set", function () {
-    store.dispatch(resetState());
+    reducer(undefined, resetState());
 
     // valid cases
-    store.dispatch(setTransferEntityType("contacts"));
-    expect(store.getState().transferButton.transferEntityType).toBe("contacts");
+    expect(
+      reducer(undefined, setTransferEntityType("contacts")).transferEntityType
+    ).toBe("contacts");
 
-    store.dispatch(setTransferEntityType("all"));
-    expect(store.getState().transferButton.transferEntityType).toBe("all");
+    expect(
+      reducer(undefined, setTransferEntityType("all")).transferEntityType
+    ).toBe("all");
 
-    store.dispatch(setTransferEntityType("leads"));
-    expect(store.getState().transferButton.transferEntityType).toBe("leads");
+    expect(
+      reducer(undefined, setTransferEntityType("leads")).transferEntityType
+    ).toBe("leads");
 
-    store.dispatch(setTransferEntityType("deals"));
-    expect(store.getState().transferButton.transferEntityType).toBe("deals");
-  });
-
-  test("check incorrect values for transferEntityType cannot be set", function () {
-    store.dispatch(resetState());
-    // @ts-expect-error
-    store.dispatch(setTransferEntityType("deal"));
-    expect(store.getState().transferButton.transferEntityType).toBe(
-      transferButtonSlice.getInitialState().transferEntityType
-    );
-
-    // @ts-expect-error
-    store.dispatch(setTransferEntityType("something"));
-    expect(store.getState().transferButton.transferEntityType).toBe(
-      transferButtonSlice.getInitialState().transferEntityType
-    );
-
-    // @ts-expect-error
-    store.dispatch(setTransferEntityType(""));
-    expect(store.getState().transferButton.transferEntityType).toBe(
-      transferButtonSlice.getInitialState().transferEntityType
-    );
+    expect(
+      reducer(undefined, setTransferEntityType("deals")).transferEntityType
+    ).toBe("deals");
   });
 });
 
 describe("check transferType manipulations", function () {
   test("transferType proper values can be set", function () {
-    store.dispatch(setTransferType("responsible"));
-    expect(store.getState().transferButton.transferType).toBe("responsible");
-    store.dispatch(setTransferType("country"));
-    expect(store.getState().transferButton.transferType).toBe("country");
+    expect(
+      reducer(undefined, setTransferType("responsible")).transferType
+    ).toBe("responsible");
+
+    expect(reducer(undefined, setTransferType("country")).transferType).toBe(
+      "country"
+    );
   });
 });
